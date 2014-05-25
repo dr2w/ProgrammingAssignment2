fecf2e92abf4c45072850924c75653c0aacf685b
Dale Webster <ddrrww@gmail.com>
2014-05-24 23:51:54 -0700

    Implements the solution to assignment 2
    Adds comments and valid code for makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions implement a wrapper for a matrix that will cache the 
+## invert operation on the matrix. 
 
-## Write a short comment describing this function
-
+## makeCacheMatrix wraps a matrix with functions supporting cached access
+## to the inverted form of the matrix.
+## get/set will get or set the value of the wrapped matrix, invalidating the cache.
+## get/setinverse will get or set the value of the cached inverted matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    getinverse <- function() inverse
+    setinverse <- function(i) inverse <<- i
+    get <- function() x
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }    
+    list(getinverse = getinverse,
+         setinverse = setinverse,
+         get = get, set = set)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve takes a wrapped matrix generated using makeCacheMatrix above and
+## either computes, caches and returns the inverted form, if necessary, or
+## simply returns the cached inverted form when available.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("retrieving cached inverted matrix")
+        return(i)
+    }
+    i <- solve(x$get(), ...)
+    x$setinverse(i)
+    i
 }
